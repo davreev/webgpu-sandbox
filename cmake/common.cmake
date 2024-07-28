45560d86d4c953db8f6e0a28c6262741a0654c4b
David Reeves <reevdav@gmail.com>
2024-07-28 21:44:26 +0100

    Fix web build with multi-config generator
    Also tidy common CMake helper functions

---
+++ cmake/common.cmake
@@ -0,0 +1,36 @@
+function(get_default_runtime_output_dir result_)
+    if(CMAKE_CONFIGURATION_TYPES)
+        set(result "${CMAKE_CURRENT_BINARY_DIR}/\$<CONFIG>")
+    else()
+        set(result "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+    set(${result_} ${result} PARENT_SCOPE)
+endfunction()
+
+
+function(copy_web_src)
+    set(src_dir "${CMAKE_CURRENT_SOURCE_DIR}")
+    get_default_runtime_output_dir(dst_dir)
+
+    set(files_out)
+    foreach(file_in ${web_src_files})
+        file(RELATIVE_PATH rel_path "${src_dir}" ${file_in})
+        set(file_out "${dst_dir}/${rel_path}")
+        add_custom_command(
+            OUTPUT
+                ${file_out}
+            DEPENDS 
+                ${file_in}
+            COMMAND 
+                ${CMAKE_COMMAND} -E copy 
+                ${file_in}
+                ${file_out}
+            COMMENT 
+                "Copying ${file_in}"
+        )
+        list(APPEND files_out ${file_out})
+    endforeach()
+
+    add_custom_target(${app_name}-web DEPENDS ${files_out})
+    add_dependencies(${app_name} ${app_name}-web)
+endfunction()
