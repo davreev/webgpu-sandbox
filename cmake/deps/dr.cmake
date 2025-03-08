19decf7b1bdae3ee154d3b02b02fe1615b74f5ed
David Reeves <reevdav@gmail.com>
2025-03-08 17:56:20 +0000

    Update dr to 0.4.0

--- cmake/deps/dr.cmake
+++ cmake/deps/dr.cmake
@@ -4,17 +4,9 @@ endif()
 
 include(FetchContent)
 
-# NOTE(dr): Pinned to specific revision until 0.4.0
-#[[
 FetchContent_Declare(
     dr
-    URL https://github.com/davreev/dr/archive/refs/tags/0.3.0.zip
-)
-]]
-FetchContent_Declare(
-    dr
-    GIT_REPOSITORY https://github.com/davreev/dr.git
-    GIT_TAG 8f301117c336123fd0b4dc0e2bf72c32607b901a
+    URL https://github.com/davreev/dr/archive/refs/tags/0.4.0.zip
 )
 
 FetchContent_MakeAvailable(dr)
