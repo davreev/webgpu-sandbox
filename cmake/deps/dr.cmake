e027fcc5b1e5c9b1686daa86379475b08aa10cc1
David Reeves <reevdav@gmail.com>
2025-09-15 03:34:45 +0100

    Update dr to 0.5.0

--- cmake/deps/dr.cmake
+++ cmake/deps/dr.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 
 FetchContent_Declare(
     dr
-    URL https://github.com/davreev/dr/archive/refs/tags/0.4.0.zip
+    URL https://github.com/davreev/dr/archive/refs/tags/0.5.0.zip
 )
 
 FetchContent_MakeAvailable(dr)
